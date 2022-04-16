b0b0f5220a564afc0b97461308bb9cb9bfb00948
Kadachikova Ekaterina <kadachikova_1996@mail.ru>
2022-04-16 22:49:46 +0400

    DZ 1lesson

---
+++ Задача1.cs
@@ -0,0 +1,9 @@
+﻿// Вывод макимального числа из двух
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max= a;
+if (b > a)
+{
+    Console.WriteLine($"Число {b} max");
+}
+
