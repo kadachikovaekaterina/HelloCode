e3a2fbb8098037944c11bb41730013801597fd22
kadachikovaekaterina <102862004+kadachikovaekaterina@users.noreply.github.com>
2022-04-16 23:16:57 +0400

    Add files via upload

---
+++ Задача2.cs
@@ -0,0 +1,8 @@
+﻿// Максимум из трех чисел
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max= a;
+if (b > a) max = b;
+if (c > b) max = c;
+System.Console.WriteLine($"Максимальное число {max}");
