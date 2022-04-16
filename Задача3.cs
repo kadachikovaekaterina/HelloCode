2a60795b7bcbbf5a85709a1bd2c5511600f3da1c
kadachikovaekaterina <102862004+kadachikovaekaterina@users.noreply.github.com>
2022-04-16 23:56:00 +0400

    Add files via upload

---
+++ Задача3.cs
@@ -0,0 +1,11 @@
+﻿// Четное/нечетное
+int a = int.Parse(Console.ReadLine());
+if (a% 2 == 0)
+{
+    System.Console.WriteLine($"{a} число четное");
+}
+else
+{
+    System.Console.WriteLine($"{a} число не четное");
+}
+
